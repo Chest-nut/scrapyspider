a973c609e7450328b83c01530aea2677397fd4b6
jikexuyuan <383105690@qq.com>
2017-10-07 00:08:45 +0800

    add requests login

---
+++ scrapy_spider/utils/zhihu_login_requests.py
@@ -0,0 +1,62 @@
+# -*- coding:utf-8 -*-
+
+try:
+    import cookielib
+except:
+    import http.cookiejar as cookielib
+import re
+
+import requests
+
+session = requests.session()
+session.cookies = cookielib.LWPCookieJar(filename='cookies.txt')
+
+user_agent = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'
+headers = {
+    'User-Agent': user_agent
+}
+
+def is_login():
+    inbox_url = 'https://www.zhihu.com/inbox/'
+    response = session.get(inbox_url, headers=headers, allow_redirects=False)
+    if response.status_code is 200:
+        return True
+    else:
+        return False
+
+def get_index():
+    try:
+        session.cookies.load(ignore_discard=True)
+        print('cookie加载成功')
+    except:
+        print('cookie加载失败！')
+    response = session.get('https://www.zhihu.com/', headers=headers)
+    with open('index.html', 'wb') as f:
+        f.write(response.text.encode('utf-8'))
+    print('ok')
+
+def get_xsrf():
+    response = session.get('https://www.zhihu.com/', headers=headers)
+    result = re.search('.*name="_xsrf" value="(.*?)"', response.text)
+    if result:
+        return result.group(1)
+    else:
+        return ''
+
+def zhihu_login(account, password):
+
+    post_data = {
+        '_xsrf': get_xsrf(),
+        'account': account,
+        'password': password
+    }
+    if re.match(r'^1\d{10}', account):
+        print('手机登录')
+        post_url = 'https://www.zhihu.com/login/phone_num'
+    else:
+        if '@' in account:
+            print('邮箱登录')
+        post_url = 'https://www.zhihu.com/login/email'
+    response = session.post(post_url, data=post_data, headers=headers)
+    print(response.text)
+    session.cookies.save()
