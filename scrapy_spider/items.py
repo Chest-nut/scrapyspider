16b3732926b007243ff83d4be54aba0e86a3932e
jikexuyuan <383105690@qq.com>
2017-10-06 13:28:00 +0800

    add itemLoader

--- scrapy_spider/items.py
+++ scrapy_spider/items.py
@@ -5,6 +5,11 @@
 # See documentation in:
 # http://doc.scrapy.org/en/latest/topics/items.html
 
+from datetime import datetime
+import re
+
+from scrapy.loader import ItemLoader
+from scrapy.loader.processors import Join, MapCompose, TakeFirst
 import scrapy
 
 
@@ -13,16 +18,57 @@ class ScrapySpiderItem(scrapy.Item):
     # name = scrapy.Field()
     pass
 
+def date_convert(value):
+    try:
+        create_date = datetime.strptime(value, "%Y/%m/%d").date()
+    except Exception:
+        create_date = datetime.now().date()
+    return create_date
+
+def remove_comment_tags(value):
+    if '评论' in value:
+        return ''
+    else:
+        return value
+
+def get_nums(value):
+    match_re = re.match(r'.*?(\d+).*', value)
+    if match_re:
+        return int(match_re.group(1))
+    else:
+        return 0
+
+def no_change(value):
+    """用于覆盖default_output_processor"""
+    return value
+
+class JobboleItemLoader(ItemLoader):
+    """自定义 ItemLoader，重载了 default_output_processor"""
+    default_output_processor = TakeFirst()
 
 class JobboleItem(scrapy.Item):
-    title = scrapy.Field()
-    create_date = scrapy.Field()
-    tags = scrapy.Field()
     url = scrapy.Field()
+    title = scrapy.Field()
     url_id = scrapy.Field()
-    front_img_url = scrapy.Field()
-    front_img_path = scrapy.Field()
-    like_nums = scrapy.Field()
-    bookmark_nums = scrapy.Field()
-    comment_nums = scrapy.Field()
     content = scrapy.Field()
+    front_img_url = scrapy.Field(
+        output_processor=MapCompose(no_change)
+    )
+    front_img_path = scrapy.Field()
+
+    like_nums = scrapy.Field(
+        input_processor=MapCompose(get_nums)
+    )
+    bookmark_nums = scrapy.Field(
+        input_processor=MapCompose(get_nums)
+    )
+    comment_nums = scrapy.Field(
+        input_processor=MapCompose(get_nums)
+    )
+    create_date = scrapy.Field(
+        input_processor=MapCompose(date_convert)
+    )
+    tags = scrapy.Field(
+        input_processor=MapCompose(remove_comment_tags),
+        output_processor=Join(',')
+    )
--- scrapy_spider/spiders/jobbole.py
+++ scrapy_spider/spiders/jobbole.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
 
-from _datetime import datetime
-import re
+from datetime import datetime
 from urllib.parse import urljoin
 
 import scrapy
 from scrapy.http import Request
 
-from scrapy_spider.items import JobboleItem
+from scrapy_spider.items import JobboleItem, JobboleItemLoader
 from scrapy_spider.utils import common
 
 class JobboleSpider(scrapy.Spider):
@@ -34,47 +33,65 @@ class JobboleSpider(scrapy.Spider):
     def parse_detial(self, response):
         """提取文章的具体内容"""
 
+        # front_img_url = response.meta.get('front_img_url')  # 文章封面图地址
+        # title = response.xpath("//div[@class='entry-header']/h1/text()").extract_first()
+        # create_date = response.xpath("//div[@class='entry-meta']\
+        #                 /p/text()").extract_first().strip().replace(' ·', '')
+        # try:
+        #     create_date = datetime.strptime(create_date, "%Y/%m/%d").date()
+        # except Exception:
+        #     create_date = datetime.now().date()
+        # like_nums = response.xpath("//span[contains(@class, 'vote-post-up')]\
+        #                             /h10/text()").extract_first()
+        # if like_nums is None:
+        #     like_nums = 0
+        # else:
+        #     like_nums = int(like_nums)
+        # bookmark_nums = response.xpath("//span[contains(@class, 'bookmark-btn')]\
+        #                     /text()").extract_first().replace('收藏', '').strip()
+        # if bookmark_nums is '': # 和 is None 有所区别
+        #     bookmark_nums = 0
+        # else:
+        #     bookmark_nums = int(bookmark_nums)
+        # comment_nums = response.xpath("//span[contains(@class, 'hide-on-480')]\
+        #                     /text()").extract_first().replace('评论', '').strip()
+        # if comment_nums is '':
+        #     comment_nums = 0
+        # else:
+        #     comment_nums = int(comment_nums)
+        # content = response.xpath("//div[@class='entry']").extract_first()
+        # tag_list = response.xpath("//div[@class='entry-meta']//a/text()").extract()
+        # tag_list = [each for each in tag_list if not each.strip().endswith('评论')]
+        # tags = ','.join(tag_list)
+        #
+        # item = JobboleItem()
+        # item['title'] = title
+        # item['create_date'] = create_date
+        # item['tags'] = tags
+        # item['url'] = response.url
+        # item['url_id'] = common.get_md5(response.url)
+        # item['front_img_url'] = [front_img_url]
+        # item['like_nums'] = like_nums
+        # item['bookmark_nums'] = bookmark_nums
+        # item['comment_nums'] = comment_nums
+        # item['content'] = content
+
+        # 使用ItemLoader填充item
+        item_loader = JobboleItemLoader(item=JobboleItem(), response=response)
+        item_loader.add_xpath('title', "//div[@class='entry-header']/h1/text()")
+        item_loader.add_xpath('create_date', "//div[@class='entry-meta']/p/text()")
+        item_loader.add_xpath('tags', "//div[@class='entry-meta']//a/text()")
+        item_loader.add_xpath('like_nums',"//span[contains(@class,\
+                                'vote-post-up')]/h10/text()")
+        item_loader.add_xpath('bookmark_nums',"//span[contains(@class,\
+                                'bookmark-btn')]/text()")
+        item_loader.add_xpath('comment_nums',"//span[contains(@class,\
+                                'hide-on-480')]/text()")
+        item_loader.add_xpath('content', "//div[@class='entry']")
+        item_loader.add_value('url', response.url)
+        item_loader.add_value('url_id', common.get_md5(response.url))
         front_img_url = response.meta.get('front_img_url')  # 文章封面图地址
-        title = response.xpath("//div[@class='entry-header']/h1/text()").extract_first()
-        create_date = response.xpath("//div[@class='entry-meta']\
-                        /p/text()").extract_first().strip().replace(' ·', '')
-        try:
-            create_date = datetime.strptime(create_date, "%Y/%m/%d").date()
-        except Exception as e:
-            create_date = datetime.now().date()
-        like_nums = response.xpath("//span[contains(@class, 'vote-post-up')]\
-                                    /h10/text()").extract_first()
-        if like_nums is None:
-            like_nums = 0
-        else:
-            like_nums = int(like_nums)
-        bookmark_nums = response.xpath("//span[contains(@class, 'bookmark-btn')]\
-                            /text()").extract_first().replace('收藏', '').strip()
-        if bookmark_nums is '': # 和 is None 有所区别
-            bookmark_nums = 0
-        else:
-            bookmark_nums = int(bookmark_nums)
-        comment_nums = response.xpath("//span[contains(@class, 'hide-on-480')]\
-                            /text()").extract_first().replace('评论', '').strip()
-        if comment_nums is '':
-            comment_nums = 0
-        else:
-            comment_nums = int(comment_nums)
-        content = response.xpath("//div[@class='entry']").extract_first()
-        tag_list = response.xpath("//div[@class='entry-meta']//a/text()").extract()
-        tag_list = [each for each in tag_list if not each.strip().endswith('评论')]
-        tags = ','.join(tag_list)
+        item_loader.add_value('front_img_url', [front_img_url])
 
-        item = JobboleItem()
-        item['title'] = title
-        item['create_date'] = create_date
-        item['tags'] = tags
-        item['url'] = response.url
-        item['url_id'] = common.get_md5(response.url)
-        item['front_img_url'] = [front_img_url]
-        item['like_nums'] = like_nums
-        item['bookmark_nums'] = bookmark_nums
-        item['comment_nums'] = comment_nums
-        item['content'] = content
-
+        item = item_loader.load_item()
         yield item
